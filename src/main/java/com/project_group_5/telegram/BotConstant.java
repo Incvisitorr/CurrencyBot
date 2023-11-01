cd5e0c8cae90b45b5a47633e27dfd773726b0516
Bohdan482 <bohdanuhr@gmail.com>
2023-11-01 11:16:23 +0200

    Допрацьовані налаштування і виправлення по валюті

--- src/main/java/com/project_group_5/telegram/BotConstant.java
+++ src/main/java/com/project_group_5/telegram/BotConstant.java
@@ -1,9 +1,7 @@
 package com.project_group_5.telegram;
 
 public class BotConstant {
-    static final String BOT_NAME="currency_tan_bot";
-//            "Currency_project_group_5Bot";
-    static final String BOT_TOKEN="6738449639:AAGrXKGeeQaESrVw7bKiXL63rqjXn8DumWQ";
-//        "6658142234:AAFFffqvGHHgTCHfCH-CifYw5HGhXiyjf9A";
+    static final String BOT_NAME="Currency_project_group_5Bot";
+    static final String BOT_TOKEN="6658142234:AAFFffqvGHHgTCHfCH-CifYw5HGhXiyjf9A";
 
 }
